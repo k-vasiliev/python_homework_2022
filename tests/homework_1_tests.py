f949e0ea28fb114b9117f9c589115c273bb4c10f
Kirill Vasilev <ki.vasilyev@hh.ru>
2022-01-23 22:50:59 +0300

    add description to tests

--- tests/homework_1_tests.py
+++ tests/homework_1_tests.py
@@ -11,11 +11,11 @@ class TestStringMethods(unittest.TestCase):
         with io.StringIO() as buf, redirect_stdout(buf):
             homework_1.print_student_info(('Иван Питонов', 2001, [8, 7, 7, 9, 6], True))
             output = buf.getvalue().split("\n")
-            self.assertEqual("Студент: Питонов, Иван", output[0])
-            self.assertEqual("Возраст: 21", output[1])
-            self.assertEqual("Оценки: 8, 7, 7, 9, 6", output[2])
-            self.assertEqual("Средний бал: 7.4", output[3])
-            self.assertEqual("Повышенная стипендия: нет", output[4])
+            self.assertEqual("Студент: Питонов, Иван", output[0], "Неправильный вывод имени и фамилии")
+            self.assertEqual("Возраст: 21", output[1], "Неправильный вывод возраста")
+            self.assertEqual("Оценки: 8, 7, 7, 9, 6", output[2], "Неправильный вывод оценок")
+            self.assertEqual("Средний бал: 7.4", output[3], "Неправильный вывод среднего балла")
+            self.assertEqual("Повышенная стипендия: нет", output[4], "Неправильный вывод информации о стипенди")
 
 
 if __name__ == '__main__':
