81eb19f354b4843f7c8d60dee5ea8ddd80bc2836
gpotemkin <gpotemkin@list.ru>
2022-02-20 23:02:48 +0300

    Homework_3 Потемкин

--- tasks/homework_3/homework_3_1.py
+++ tasks/homework_3/homework_3_1.py
@@ -15,3 +15,24 @@
 """
 
 user_input = input()
+word_list = user_input.split(' ')
+
+# список уникальных слов
+unique_word = []
+for w in word_list:
+    if w in unique_word:
+        continue
+    else:
+        unique_word.append(w)
+
+# составим список списков "слово, количество"
+count_word = []
+n = 0
+for word in unique_word:
+    count_word.append([word, word_list.count(word)])
+    n += 1
+
+for i, j in count_word:
+    print( f'{i}: {j}')
+
+
