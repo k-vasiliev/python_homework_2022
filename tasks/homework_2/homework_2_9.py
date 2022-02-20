62dd18079c5f2ba3ee9cdf208408ce07540e1b3e
gpotemkin <gpotemkin@list.ru>
2022-02-20 17:47:29 +0300

    Homework_2 Потемкин

--- tasks/homework_2/homework_2_9.py
+++ tasks/homework_2/homework_2_9.py
@@ -3,10 +3,13 @@
 # Выведите его наименьший натуральный делитель, отличный от 1
 
 user_input = int(input())
-z = 2
-for z in range(z, user_input+1):
-    if user_input % z == 0:
-        print(z)
-        break
-    else:
-        z += 1
+if user_input < 2:
+    print("Число должно быть не меньше 2")
+else:
+    z = 2
+    for z in range(z, user_input+1):
+        if user_input % z == 0:
+            print(z)
+            break
+        else:
+            z += 1
