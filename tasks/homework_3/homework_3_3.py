9adea017e7c17067c4d28b0dc0bb2fa7c32987d4
gpotemkin <gpotemkin@list.ru>
2022-02-23 00:23:26 +0300

    Homework_3 Потемкин

--- tasks/homework_3/homework_3_3.py
+++ tasks/homework_3/homework_3_3.py
@@ -3,5 +3,14 @@
 Программа должна выдавать результат в виде разделенной запятыми строки
 
 """
+all_even = []
+for n in (1000, 1001):
+    n_list = [int(a) for a in str(n)]
+    for elem in n_list:
+        if elem % 2 != 0:
+            break
+    all_even.append(n)
+print(all_even)
+
 
 
