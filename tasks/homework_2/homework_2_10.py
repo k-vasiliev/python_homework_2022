ccae8ad9099eec4ee6b3cb3bdf6b064d12ba9e4f
Z5043 <99119131+Z5043@users.noreply.github.com>
2022-02-26 12:59:40 +0300

    Выполнена домашняя работа

--- tasks/homework_2/homework_2_10.py
+++ tasks/homework_2/homework_2_10.py
@@ -1,5 +1,6 @@
 """
-Группа из 3 студентов пишет заявки на желаемые факультативы из списка: английский, немецкий, право, математика, сольфеджио. Факультатив откроют, если на него запишутся все студенты. Каждый студент может выбрать минимум один и максимум три факультатива. Нужно посчитать количество факультативов, которые откроются.
+Группа из 3 студентов пишет заявки на желаемые факультативы из списка: английский, немецкий,право, математика, сольфеджио. Факультатив откроют, если на него запишутся
+все студенты. Каждый студент может выбрать минимум один и максимум три факультатива. Нужно посчитать количество факультативов, которые откроются.
 
 Пример
 
@@ -23,6 +24,21 @@
 2
 """
 
-student_1_disciplines = #
-student_2_disciplines = #
-student_3_disciplines = #
+student_1_disciplines = 'английский сольфеджио право'
+student_2_disciplines = 'математика сольфеджио'
+student_3_disciplines = 'немецкий право'
+set_student_1_disciplines = set(student_1_disciplines.split())
+set_student_2_disciplines = set(student_2_disciplines.split())
+set_student_3_disciplines = set(student_3_disciplines.split())
+
+count_disciplines = set_student_1_disciplines.intersection(set_student_2_disciplines, set_student_3_disciplines)
+print(len(count_disciplines))
+
+
+'''
+for i in list_student_1_disciplines:
+    for a in list_student_2_disciplines:
+        for b in list_student_3_disciplines:
+            print(i)
+'''
+
--- tasks/homework_2/homework_2_9.py
+++ tasks/homework_2/homework_2_9.py
@@ -2,4 +2,13 @@
 # Пользователь вводит целое число, не меньшее 2
 # Выведите его наименьший натуральный делитель, отличный от 1
 
-user_input = input()
+user_input = int(input())
+for i in range(2, user_input+1):
+    if int(user_input) % i == 0:
+        print(i)
+        break
+
+
+
+
+
