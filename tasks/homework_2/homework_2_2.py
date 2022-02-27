033670da2341f52fd3635cea1ed26e9834573b80
mamedov-onur <onur.mamedov@yandex.ru>
2022-02-27 22:51:30 +0300

    Homework2 Мамедов

--- tasks/homework_2/homework_2_2.py
+++ tasks/homework_2/homework_2_2.py
@@ -1,10 +1,9 @@
 # пользователь вводит две строки (по очереди)
-
 # Считайте ввод и распечатайте первую фразу с места, где заканчивается вторая
 # пример:
-# пользователь ввел "Hello, welcome"
-# пользователь ввел "Hello"
-# вывод ", welcome"
-
-input1 = input()
-input2 = input()
+# пользователь ввел "Hello, welcome":
+some_input1 = 'Hello, welcome'
+# пользователь ввел "Hello":
+some_input2 = 'Hello'
+# вывод ", welcome":
+print('Hello, welcome'.replace('Hello', ' '))
