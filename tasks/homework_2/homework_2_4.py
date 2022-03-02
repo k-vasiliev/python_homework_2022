389298e3f905fc39944c3763a18d068ed2e3034a
Alex_Nevski <93284775+Nevaplat@users.noreply.github.com>
2022-03-02 18:19:25 +0300

    Homework_2_Nevski

--- tasks/homework_2/homework_2_4.py
+++ tasks/homework_2/homework_2_4.py
@@ -4,3 +4,17 @@
 # то сумма будет считаться нулевой
 
 user_input = input()
+find_first_whitespace = user_input.find(' ')
+number_1 = float(user_input[:find_first_whitespace])
+remaining_data = user_input[find_first_whitespace+1:]
+
+find_second_whitespace = remaining_data.find(' ')
+number_2 = float(remaining_data[:find_second_whitespace])
+number_3 = float(remaining_data[find_second_whitespace+1:])
+
+if number_1 == number_2 or number_2 == number_3 or number_3 == number_1:
+    sum_of_numbers = 0
+else:
+    sum_of_numbers = number_1 + number_2 + number_3
+
+print(sum_of_numbers)
