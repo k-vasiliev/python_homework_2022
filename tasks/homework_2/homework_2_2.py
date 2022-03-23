f2d7827f1d28bc8db2f07cd448911ec8de851996
veronikachistyakova <chistnika>
2022-03-23 16:23:38 +0300

    homework 2 Chistyakova

--- tasks/homework_2/homework_2_2.py
+++ tasks/homework_2/homework_2_2.py
@@ -8,3 +8,15 @@
 
 input1 = input()
 input2 = input()
+# в первом вводе ищем второй ввод
+file_format = input1.find(input2)
+
+print(input1[file_format+len(input2):])
+
+
+
+
+
+
+
+
--- tasks/homework_2/homework_2_4.py
+++ tasks/homework_2/homework_2_4.py
@@ -4,3 +4,15 @@
 # то сумма будет считаться нулевой
 
 user_input = input()
+# Формируем эти числа в список
+user_list = user_input.split(' ')
+# Переводим список в числа
+integer_map = map(int, user_list)
+integer_list = list(integer_map)
+# Преобразуем в множество
+integer_list_set = set(integer_list)
+if len(integer_list_set) < len(integer_list):
+    print(0)
+else:
+    summ = sum(integer_list)
+    print(summ)
