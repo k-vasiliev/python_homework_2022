c2f1d4b1e90d8b4e5239507480edf213e255603f
gpotemkin <gpotemkin@list.ru>
2022-03-24 14:54:02 +0300

    Homework_4_Потемкин

--- tasks/homework4/homework_4_1.py
+++ tasks/homework4/homework_4_1.py
@@ -3,3 +3,14 @@
 Она должна возвращать список из чисел. Используйте типизацию!
 """
 
+
+def fibonacci(n: int):
+    fib = [1, 1]
+    for _ in range(1, n-1):
+        fib_n = int(fib[-2]) + int(fib[-1])
+        fib.append(fib_n)
+    return fib
+
+
+n_input = int(input())
+print(*fibonacci(n_input), sep = ', ')
