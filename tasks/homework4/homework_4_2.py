512a0acd153c7b25a11b577a2fccf7a7007e0ab2
Juli-Kareva <y.kareva007@gmail.com>
2022-03-27 20:15:47 +0300

    Homework_4 Kareva

--- tasks/homework4/homework_4_2.py
+++ tasks/homework4/homework_4_2.py
@@ -7,14 +7,20 @@
 Примените декоратор к example_function
 """
 
+import datetime
 
 def debug(func):
     pass
-
+    def wrapper(*args):
+        print (args)
+        print (f'Начало: {datetime.datetime.now()}')
+        print(func(*args))
+        print(f'Конец: {datetime.datetime.now()}')
+    return wrapper
 
 # к этой функции надо применить декоратор
+@debug
 def example_function(argument):
     return argument[::-1]
-
 
 example_function("Hello! Debug me, please")
