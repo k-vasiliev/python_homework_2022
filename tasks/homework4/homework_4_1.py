ec8914cc5e9a433bfb2bfcea0eb2ca65da09d52e
Juli-Kareva <y.kareva007@gmail.com>
2022-03-27 19:14:56 +0300

    Homework_4 Kareva

--- tasks/homework4/homework_4_1.py
+++ tasks/homework4/homework_4_1.py
@@ -3,3 +3,18 @@
 Она должна возвращать список из чисел. Используйте типизацию!
 """
 
+def fibonacci(N: int):
+    fibonachi_list = [1, 1]
+    if len(fibonachi_list) < N:
+        while len(fibonachi_list) < N:
+            extra = fibonachi_list[-1] + fibonachi_list[-2]
+            fibonachi_list.append(extra)
+    elif N == 2:
+        fibonachi_list = [1, 1]
+    else:
+        fibonachi_list = [1]
+    return fibonachi_list
+
+N_num = int(input())
+
+print(fibonacci(N_num))
