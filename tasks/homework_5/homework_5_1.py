e841e87674de7a1951f6eccd1880dc5544ee1f0b
Juli-Kareva <y.kareva007@gmail.com>
2022-04-22 23:24:09 +0300

    Homework_5 Kareva

--- tasks/homework_5/homework_5_1.py
+++ tasks/homework_5/homework_5_1.py
@@ -6,7 +6,21 @@
 Найти информацию по работе с zip в python вам нужно самостоятельно
 
 """
+import shutil, glob
 
+def zip_images(source: str, destination: str):
+    """  pass
 
-def zip_images():
-    pass
+ source = "C:\\Users\julia\Documents\Питон_ВШЭ\img_source"
+  destination = "C:\\Users\julia\Documents\Питон_ВШЭ\img_destin" """
+
+    for file in glob.glob(source+"\*.jpg"):
+        shutil.copy2(file, destination);
+
+    shutil.make_archive(destination, 'zip', destination)
+    return print('ready')
+
+zip_images("C:\\Users\julia\Documents\Питон_ВШЭ\img_source", "C:\\Users\julia\Documents\Питон_ВШЭ\img_destin")
+"""
+
+"""
