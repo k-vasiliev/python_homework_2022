b5ebbe9a373307edfa1c21e2974e243afb9937a4
borisov-dmitriy <79031632696@yandex.ru>
2022-04-24 02:01:18 +0300

    Homework 5 Борисов Дмитрий

--- tasks/homework_5/homework_5_4.py
+++ tasks/homework_5/homework_5_4.py
@@ -13,12 +13,32 @@
 и используете assert для тестирования
 
 """
-
-
 def bisearch(sorted_list: list[int], number_to_find) -> int:
-    pass
+    l = len(sorted_list)
+    ml = l // 2
+    m_start = 0
 
+    while len(sorted_list) > 1:
+        #уходим в левую часть массива
+        if number_to_find < sorted_list[ml]:
+            sorted_list = sorted_list[:ml]
+            l = len(sorted_list)
+            ml = l // 2
+        #уходим в правую часть массива
+        else:
+            sorted_list = sorted_list[ml:]
+            l = len(sorted_list)
+            m_start = m_start + ml
+            ml = l // 2
 
-assert bisearch([1, 2, 3], 2) == 1
+    if sorted_list[0] != number_to_find:
+        return None
+    return m_start
+
+print(bisearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 15))
+
+assert bisearch([1, 2, 3], 3) == 2
 assert bisearch([1, 2, 3], 5) is None
+assert bisearch([-1, 2, 3], -1) == 0
+assert bisearch([1, 1, 1, 2, 3, 4], 1) == 2 #можно упороться, чтобы находило первое вхождение искомого числа, но кажется это избыточно, если нужно было так - пиши
 # рекомендую дописать сюда еще проверок функций на пограничные ситуации
