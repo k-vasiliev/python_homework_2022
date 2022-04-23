2dac8689801be64d1c628bfcfc1d62f9f715e909
Iskander-Daraev <99560093+Iskander-Daraev@users.noreply.github.com>
2022-04-23 23:24:10 +0300

    Update homework_5_1.py

--- tasks/homework_5/homework_5_1.py
+++ tasks/homework_5/homework_5_1.py
@@ -6,17 +6,3 @@
 Найти информацию по работе с zip в python вам нужно самостоятельно
 
 """
-
-def zip_images():
-    pass
-
-import os
-import zipfile
-
-zip_images = zipfile.ZipFile('zip_images.zip', 'w', zipfile.ZIP_DEFLATED)
-suffix = ('.jpeg', '.png')
-for folder, subfolders, files in os.walk('C://Users//q2364//Desktop//python_homework_2022//tasks//homework_5//data'):
-    for file in files:
-        if file.endswith(suffix):
-            zip_images.write(os.path.join (folder, file), os.path.relpath (os.path.join(folder, file), 'C://Users//q2364//Desktop//python_homework_2022//tasks//homework_5//data'))
-zip_images.close()
