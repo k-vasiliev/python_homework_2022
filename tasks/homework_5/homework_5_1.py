67dc0663ce9a9bd09c6968c51049e6d0022f8529
gpotemkin <gpotemkin@list.ru>
2022-04-23 17:42:36 +0300

    Homework_5_Потемкин

--- tasks/homework_5/homework_5_1.py
+++ tasks/homework_5/homework_5_1.py
@@ -6,7 +6,23 @@
 Найти информацию по работе с zip в python вам нужно самостоятельно
 
 """
+import os
+import zipfile
 
 
-def zip_images():
-    pass
+def zip_images(folder):
+# создадим кортеж из списка возможных расширений и найдем все файлы с данными расширениями
+    images_extensions = (".png", ".jpeg", ".tiff", ".gif", ".jpg", ".bmp", ".wmf")
+# создадим архив
+    z = zipfile.ZipFile('images.zip', 'w')
+# ищем все файлы-изображения и добавляем в архив
+    for file in os.listdir(folder):
+        if file.endswith(images_extensions):
+            print(os.path.join(file))
+            z.write(os.path.join(folder, file))
+    z.close()
+
+zip_images(".\data")
+
+
+
