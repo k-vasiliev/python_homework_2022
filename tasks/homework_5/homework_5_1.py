facd4e08c5b5ede92080ae163fd1d8755dba06f2
Z5043 <99119131+Z5043@users.noreply.github.com>
2022-04-24 19:47:31 +0300

    homework_5_Izakov

--- tasks/homework_5/homework_5_1.py
+++ tasks/homework_5/homework_5_1.py
@@ -8,5 +8,27 @@
 """
 
 
+"""
+В папке data лежат файлы
+Ваша задача создать zip архив, в котором будут лежать только изображения из этой папки
+
+Определять является ли файл изображением нужно по расширению
+Найти информацию по работе с zip в python вам нужно самостоятельно
+
+"""
+
+import os
+from zipfile import ZipFile
+from os import path
+
 def zip_images():
-    pass
+    images_format = {'jpg', 'png', 'bmp', 'ai', 'psd', 'ico', 'jpeg', 'ps', 'svg', 'tif',
+                  'tiff'}
+    for i in os.listdir('data'):
+        if i.split('.')[-1] in images_format:
+            srs_path = path.join('data',i)
+            src = path.realpath(srs_path)
+            root_dir,tail = path.split(src)
+            with ZipFile("archive_images.zip", "a") as newzip:
+                newzip.write(srs_path)
+
--- tasks/homework_5/homework_5_2.py
+++ tasks/homework_5/homework_5_2.py
@@ -10,7 +10,27 @@
 В 2006 процент женщин попадающих в эту группу - 14082/59193=23.78%
 Изменение на 0.11%
 """
+import csv
 
 
-def find_mothers_age_diff(year1: int, year2: int, age_group: str):
-    print(f'Разница в группе {age_group} составляет {}%. В {year1} процентное соотношение было {}%, а в {year2} стало {}%')
+def find_mothers_age_diff(first_year: int, second_year: int, age_group: str):
+    with open(r'data\births-by-mothers-age.csv', 'r', encoding='utf8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            if row['Period'] == str(first_year) and row['Mothers_Age'] == 'Total':
+                total_first_year = int(row['Count'])
+            if row['Period'] == str(second_year) and row['Mothers_Age'] == 'Total':
+                total_second_year = int(row['Count'])
+            if row['Period'] == str(first_year) and row['Mothers_Age'] == age_group:
+                age_group_first_year = int(row['Count'])
+            if row['Period'] == str(second_year) and row['Mothers_Age'] == age_group:
+                age_group_second_year = int(row['Count'])
+
+        first_year_value = round(age_group_first_year / total_first_year * 100, 2)
+        second_year_value = round(age_group_second_year / total_second_year * 100, 2)
+        delta = round(first_year_value - second_year_value, 2)
+        print(f'Разница в группе {age_group} составляет {delta}%. В {first_year} процентное соотношение было '
+              f'{first_year_value}%, а в {second_year} стало {second_year_value}%')
+
+
+find_mothers_age_diff(2006, 2005, '25–29')
