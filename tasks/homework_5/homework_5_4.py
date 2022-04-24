43f43051b8375ef4a129d0bf23512e3da507fcf9
borisov-dmitriy <79031632696@yandex.ru>
2022-04-24 19:10:52 +0300

    Homework 5 Борисов Дмитрий

--- tasks/homework_5/homework_5_4.py
+++ tasks/homework_5/homework_5_4.py
@@ -35,7 +35,7 @@ def bisearch(sorted_list: list[int], number_to_find) -> int:
         return None
     return m_start
 
-print(bisearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 15))
+print(bisearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 3))
 
 assert bisearch([1, 2, 3], 3) == 2
 assert bisearch([1, 2, 3], 5) is None
