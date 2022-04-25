3e3babb63b42ceb16d3ac0f2b0c63781c0a0686e
gpotemkin <gpotemkin@list.ru>
2022-04-25 16:07:29 +0300

    Homework_5_Потемкин

--- tasks/homework_5/homework_5_2.py
+++ tasks/homework_5/homework_5_2.py
@@ -10,7 +10,26 @@
 В 2006 процент женщин попадающих в эту группу - 14082/59193=23.78%
 Изменение на 0.11%
 """
+import os
+import csv
+
+year1 = 2005
+year2 = 2006
+age_group = '25–29'
+ttt = []
+
+with open("./data/births-by-mothers-age.csv", encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    #Period, Mothers, Count, *_ = reader.fieldnames
+    for row in reader:
+        if row['Period'] == year1 and row['Mothers_Age'] == age_group:
+           ttt.append(row['Count'])
+           print(ttt)
+        #if row['Period'] == year2 and row['Mothers_Age'] == age_group:
+           # count2 = row['Count']
 
 
+"""
 def find_mothers_age_diff(year1: int, year2: int, age_group: str):
-    print(f'Разница в группе {age_group} составляет {}%. В {year1} процентное соотношение было {}%, а в {year2} стало {}%')
+        print(f'Разница в группе {age_group} составляет {}%. В {year1} процентное соотношение было {}%, а в {year2} стало {}%')
+"""
