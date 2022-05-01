b4c6198d9d12bd5496ed9fc95c61431125a6f5fa
Juli-Kareva <y.kareva007@gmail.com>
2022-05-01 10:49:16 +0300

    Homework_5 Kareva

--- tasks/homework_5/homework_5_2.py
+++ tasks/homework_5/homework_5_2.py
@@ -9,8 +9,31 @@
 В 2005 процент женщин попадающих в эту группу - 13797/57747=23.89%
 В 2006 процент женщин попадающих в эту группу - 14082/59193=23.78%
 Изменение на 0.11%
+
 """
+import csv
+def find_mothers_age_diff (year1: int, year2: int, age_group: str):
 
+    with open(r'C:\Users\julia\Documents\Питон_ВШЭ\births-by-mothers-age.csv') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            if row['Period'] == str(year1) and row['Mothers_Age'] == str(age_group):
+                count_year1 = int(row['Count'])
+            elif row['Period'] == str(year1) and row['Mothers_Age'] == str('Total'):
+                total_year1 = int(row['Count'])
+            elif row['Period'] == str(year2) and row['Mothers_Age'] == str(age_group):
+                count_year2 = int(row['Count'])
+            elif row['Period'] == str(year2) and row['Mothers_Age'] == str('Total'):
+                total_year2 = int(row['Count'])
+            else:
+                print ('такой информации нет')
+                break
 
-def find_mothers_age_diff(year1: int, year2: int, age_group: str):
-    print(f'Разница в группе {age_group} составляет {}%. В {year1} процентное соотношение было {}%, а в {year2} стало {}%')
+        percent_year1 = round((count_year1 / total_year1) * 100, 2)
+        percent_year2 = round((count_year2 / total_year2) * 100, 2)
+        difference = abs(percent_year1 - percent_year2)
+
+    return print(f'Разница в группе {age_group} составляет {difference}%. В {year1} процентное соотношение было {percent_year1}%, а в {year2} стало {percent_year2}%')
+
+find_mothers_age_diff(2005, 2006, '25–29')
+
--- tasks/homework_5/homework_5_3.py
+++ tasks/homework_5/homework_5_3.py
@@ -3,3 +3,22 @@
 Распарсите ее значения и сохраните в csv файл с названием 'man_oscarcs.csv'
 
 """
+
+from bs4 import BeautifulSoup
+import csv
+
+with open("./data/oscar.html", "r") as f:
+    contents = f.read()
+    soup = BeautifulSoup(contents, "html.parser")
+    rows = soup.find_all("tr")
+
+with open("man_oscarcs.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    for row in rows:
+        csv_row = []
+        for cell in row.findAll(["td", "th"]):
+            csv_row.append(cell.get_text())
+        writer.writerow(csv_row)
+
+
+
